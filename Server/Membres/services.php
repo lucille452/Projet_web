55b5a84567173fb89f0a1fe8e35f55f5acb81922
Sasha <sasha.brun@ynov.com>
2024-04-26 09:04:37 +0200

    page change

--- Server/Membres/services.php
+++ Server/Membres/services.php
@@ -4,7 +4,7 @@ function addMembre($bdd, $nom, $prenom, $mail, $dateNaissance, $mdp) {
     $newMembre = $bdd->prepare("INSERT INTO membres(nom, prenom, adresse_mail, date_naissance, mot_de_passe) VALUES (?,?,?,?,?) ");
     $newMembre->execute([$nom, $prenom, $mail, $dateNaissance, $mdp]);
 
-    header("Location: accueil_membre.php");
+    header("Location: ../Member_page/accueil_membre.php");
 }
 
 function deleteMembre($bdd, $id) {
@@ -16,5 +16,5 @@ function addAdmin($bdd, $nom, $prenom, $mail, $dateNaissance, $mdp) {
     $newAdmin = $bdd->prepare("INSERT INTO admin(nom, prenom, adresse_mail, date_naissance, mot_de_passe) VALUES (?,?,?,?,?) ");
     $newAdmin->execute([$nom, $prenom, $mail, $dateNaissance, $mdp]);
 
-    header("Location: accueil_membre.php");
+    header("Location: ../Admin_page/accueil_admin.php");
 }
