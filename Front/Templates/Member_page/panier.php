93f27cf8321f1e79ded359ef156b27ed2e92e30a
lucille <lucille.cenac@ynov.com>
2024-05-15 12:16:36 +0200

    page panier check little

--- Front/Templates/Member_page/panier.php
+++ Front/Templates/Member_page/panier.php
@@ -36,46 +36,21 @@
                 <div class="row">
                     <div class="col-md-12 col-lg-8">
                         <div class="items">
-                            <div class="product">
-                                <div class="row">
-                                    <div class="col-md-3">
-                                        <img class="img-fluid mx-auto d-block image" src="assets/img/image.jpg">
-                                    </div>
-                                    <div class="col-md-8">
-                                        <div class="info">
-                                            <div class="row">
-                                                <div class="col-md-5 product-name">
-                                                    <div class="product-name">
-                                                        <a href="#">Lorem Ipsum dolor</a>
-                                                        <div class="product-info">
-                                                            <div>Display: <span class="value">5 inch</span></div>
-                                                            <div>RAM: <span class="value">4GB</span></div>
-                                                            <div>Memory: <span class="value">32GB</span></div>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                                <div class="col-md-4 quantity">
-                                                    <label for="quantity">Quantity:</label>
-                                                    <input id="quantity" type="number" value ="1" class="form-control quantity-input">
-                                                </div>
-                                                <div class="col-md-3 price">
-                                                    <span>$120</span>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
+                            <?php
+                            include '../../../Server/Articles/services.php';
+                            $bdd = new PDO('mysql:host=localhost;dbname=projet_dev;charset=utf8','root','');
+
+                            getArticles($bdd);
+                            ?>
                         </div>
                     </div>
                     <div class="col-md-12 col-lg-4">
                         <div class="summary">
-                            <h3>Summary</h3>
-                            <div class="summary-item"><span class="text">Subtotal</span><span class="price">$360</span></div>
-                            <div class="summary-item"><span class="text">Discount</span><span class="price">$0</span></div>
-                            <div class="summary-item"><span class="text">Shipping</span><span class="price">$0</span></div>
-                            <div class="summary-item"><span class="text">Total</span><span class="price">$360</span></div>
-                            <button type="button" class="btn btn-primary btn-lg btn-block">Checkout</button>
+                            <h3>Récapitulatif de la commande</h3>
+                            <div class="summary-item"><span class="text">Sous total</span><span class="price"><?php echo getTotalPrix($bdd)?>€</span></div>
+                            <div class="summary-item"><span class="text">Remise</span><span class="price">0€</span></div>
+                            <div class="summary-item"><span class="text">Total</span><span class="price"><?php echo getTotalPrix($bdd)?>€</span></div>
+                            <button type="button" class="btn btn-primary btn-lg btn-block">Payer</button>
                         </div>
                     </div>
                 </div>
